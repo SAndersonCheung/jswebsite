e35f312f1721f976b08f54816135457ce5ad3ac1
SAndersonCheung <45010157+SAndersonCheung@users.noreply.github.com>
2019-08-17 20:06:18 +0100

    oof

---
+++ colourgradient.js
@@ -0,0 +1,9 @@
+function makeGradient(){
+    var colourOne = null;
+    var colourTwo = null;
+    document.getElementById('bigbox').style.background='linear-gradient(to right, colourOne, colourTwo)';
+}
+
+function retrieveColour(){
+    
+}
