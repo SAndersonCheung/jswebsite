41a63d6ddb5eecc94937391689e3ed6f945591b9
SAndersonCheung <45010157+SAndersonCheung@users.noreply.github.com>
2019-08-20 15:44:27 +0100

    Add files via upload

--- colourgradient.js
+++ colourgradient.js
@@ -1,9 +1,34 @@
 function makeGradient(){
     var colourOne = null;
     var colourTwo = null;
-    document.getElementById('bigbox').style.background='linear-gradient(to right, colourOne, colourTwo)';
+    document.getElementById('bigbox').style.background = 'linear-gradient(to right, retrieveColour(colourOne), retrieveColour(colourTwo))';
 }
 
-function retrieveColour(){
-    
+function retrieveColour(cat){
+    return document.getElementById(cat).style.backgroundColor;
+}
+
+function setOne(){
+    var colour = one;
+}
+function setTwo(){
+    var colour = two;
+}
+function setThree(){
+    var colour = three;
+}
+function setFour(){
+    var colour = four;
+}
+function setFive(){
+    var colour = five;
+}
+function setSix(){
+    var colour = six;
+}
+function setSeven(){
+    var colour = seven;
+}
+function setEight(){
+    var colour = eight;
 }
