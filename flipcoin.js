f9e8da1ed486a229be58f4f0b76dc113c28c4b88
SAndersonCheung <45010157+SAndersonCheung@users.noreply.github.com>
2019-10-10 12:00:58 +0100

    coin flip changes
    x changes result of coin flip to say head or tail
    x added image

--- flipcoin.js
+++ flipcoin.js
@@ -2,5 +2,14 @@ var result = null;
 
 function flipCoin() {
     result = Math.floor( Math.random() * 2 ) + 1;
-    document.getElementById('coinVal').innerHTML = result;
+    
+    if (result == 1){
+        document.getElementById('coinVal').innerHTML = "Heads";
+        document.getElementById('resultImg').src="/resources/heads.jpg";
+    } else if (result == 2){
+        document.getElementById('coinVal').innerHTML = "Tails";
+        document.getElementById('resultImg').src="/resources/tails.jpg";
+    }
+    
+    
 }
