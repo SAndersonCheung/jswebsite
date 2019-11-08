45c5502d68323f271ea4e101466fffe89498e13a
SAndersonCheung <45010157+SAndersonCheung@users.noreply.github.com>
2019-11-08 10:38:46 +0000

    js: maths
    custom power option

--- maths.js
+++ maths.js
@@ -2,16 +2,26 @@
 function squared(){
     let inp = document.getElementById("input").value;
     let result = inp * inp;
-    document.getElementById('squared').innerHTML = result;
+    document.getElementById("squared").innerHTML = result;
 }
 
 function cubed(){
     let inp = document.getElementById("input").value;
     let result = inp * inp * inp;
-    document.getElementById('cubed').innerHTML = result;
+    document.getElementById("cubed").innerHTML = result;
 }
 
+
 function cusPow(){
+    let inp = document.getElementById("input").value;
+    let count = document.getElementById("custom").value;
+    let result = inp;
+    
+    for (i = 0; i < count; i++){
+        result = result * inp;
+    }
+    
+    document.getElementById('cuspower').innerHTML = result;
 }
 
 // 
