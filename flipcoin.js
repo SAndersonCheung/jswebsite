be50f31fab8908214290b1d0afa7116ba76ce72b
SAndersonCheung <45010157+SAndersonCheung@users.noreply.github.com>
2019-11-14 10:04:24 +0000

    js : flip
    light formatting and good practise changes

--- flipcoin.js
+++ flipcoin.js
@@ -1,14 +1,15 @@
 var result = null;
 
 function flipCoin() {
-    result = Math.floor( Math.random() * 2 ) + 1;
+    result = Math.floor(Math.random() * 2) + 1;
     
-    if (result == 1){
+    if (result === 1) {
         document.getElementById('coinVal').innerHTML = "Heads";
-        document.getElementById('resultImg').src="./heads.jpg";
-    } else if (result == 2){
+        document.getElementById('resultImg').src = "./resources/heads.jpg";
+    } else if (result === 2) {
         document.getElementById('coinVal').innerHTML = "Tails";
-        document.getElementById('resultImg').src="./tails.jpg";
+        document.getElementById('resultImg').src = "./resources/tails.jpg";
+        // image is delayed?
     }
     
     
