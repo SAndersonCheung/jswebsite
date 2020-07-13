74e5107ac0e579b0e41977bb64cb4496338bba70
SAndersonCheung <summer.anderson99@hotmail.co.uk>
2020-07-13 15:37:13 +0100

    works now
    - fixed the icon links on about page

--- fibon.js
+++ fibon.js
@@ -1,7 +1,7 @@
 let canvas = document.getElementById("fibCanvas");
 let ctx = canvas.getContext("2d");
 
-function calLines(){
+let calLines = () => {
     ctx.beginPath();
     ctx.moveTo(0, 0);
     for (let i = 0; i < 50; i++){
@@ -10,15 +10,15 @@ function calLines(){
     }
 }
 
-function fibo(length){
+let fibo = (length) => {
     let a = 1, b = 0, temp;
-    
+
     while (length >= 0){
         temp = a;
         a = a + b;
         b = temp;
         length--;
     }
-    
+
     return b;
 }
